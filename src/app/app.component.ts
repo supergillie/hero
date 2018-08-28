b4444a4f9d9d3043d43d3d9a24d12df8ae61c87b
Peter Krooks <pekr@Peters-MacBook-Pro.local>
2018-08-28 11:29:49 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+//import { supervillains } from 'supervillains';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  heroName1;
+  heroName2;
+  heroName3;
+  heroName4;
+  heroName5;
+
+
+
+  ngOnInit(){
+    const supervillains = require('supervillains');
+    this.heroName1 = supervillains.random();
+    this.heroName2 = supervillains.random(); 
+    this.heroName3 = supervillains.random(); 
+    this.heroName4 = supervillains.random(); 
+    this.heroName5 = supervillains.random();  
+  }
 }
