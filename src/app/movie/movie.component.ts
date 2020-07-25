11de8a2da65b9f28edf73ca041e960129712ee9d
supergillie <peter.krooks@gmail.com>
2020-07-25 13:08:30 +0200

    movieflik klar

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TopListWordsService } from './../top-list-words.service';
+import { ResourceLoader } from '../../../node_modules/@angular/compiler';
+
+declare var require: any;
 
 @Component({
   selector: 'app-movie',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieComponent implements OnInit {
 
-  constructor() { }
+  heroName1; heroName2; heroName3; heroName4; heroName5;
+  titelord1; titelord2; titelord3; titelord4;
+
+  htmlstring = "https://api.wordnik.com/v4/words.json/randomWords?hasDictionaryDef=false&includePartOfSpeech=noun&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=1&maxLength=-1&limit=20&api_key=1e76922a1e9f2542dc0050e7c5903f8c3f52c1c2f5c4d1f5c"
+  toplisteord;
+  toplisteord2;
+  skurknamn;
+  ord;
+  ord2;
+  rows = ['', ''];
+  row2s = ['', ''];
+
+  constructor(private topListWordsService: TopListWordsService) { }
 
   ngOnInit() {
-  }
+    const supervillains = require('supervillains');
+      this.ord = this.topListWordsService.getOdessaWords(1);
+      this.titelord2 = this.ord[1]
+      this.titelord1 = "Odessa"
+      this.toplisteord = this.topListWordsService.getOdessaWords(50);
 
+      for (var i = 0; i < 5; i++) {
+        this.rows[i] = this.toplisteord[i]
+          + ' ' + this.toplisteord[20 + i]
+          + ' ' + this.toplisteord[21 + i]
+          + ' ' + this.toplisteord[22 + i];
+      }
+    };
+  }
 }
